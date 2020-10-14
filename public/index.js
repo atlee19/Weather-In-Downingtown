63a539aa530a8ca8a8dbd6602c513fdf03ade962
atlee19 <atleegraham16@gmail.com>
2020-10-14 13:30:01 -0400

    nodemon isntalled && weather desc is being fetched

--- public/index.js
+++ public/index.js
@@ -4,4 +4,8 @@ const key = "758d03a426da12aba369174e1675ed88"
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 .then(res => res.json())
-.then(data => console.log(data.weather))
+.then(data => {
+    const weather = data.weather[0]
+    console.log(weather.description)
+    
+})
