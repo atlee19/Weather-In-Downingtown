ce20e5c766b859bf16a85f069e3a6ac7e92b9a00
atlee19 <atleegraham16@gmail.com>
2020-10-15 12:30:36 -0400

    Clouds switch function implemented

--- public/index.js
+++ public/index.js
@@ -10,17 +10,50 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     const weatherCard = document.getElementById("weather_info")
     weatherCard.innerHTML += `<h1>${city}</h1>`
     weatherCard.innerHTML += `<h3>${weather.description}</h3>`
-    setBackground(weather.description)
+    setBackground(weather.main, weather.description)
 })
 
 
-const setBackground = (weatherDesc) => {
-    switch (weatherDesc) {
-        case "clear sky":
-            document.body.style.backgroundImage = "url('clearSky.jpg')"
+const setBackground = (mainWeather, weatherDesc) => {
+    switch (mainWeather) {
+        case "Clear":
+            Clear()
+            break;
+        
+        case "Clouds":
+            Clouds(weatherDesc)
             break;
     
         default:
+            break;
+    }
+}
+
+const Clear = () => {
+    document.body.style.backgroundImage = "url('clearSky.jpg')"
+}
+
+const Clouds = (weatherDesc) => {
+    switch (weatherDesc) {
+        case "few clouds":
+            //code
+            console.log('few clouds detected!')
+            break;
+        
+        case "scattered clouds":
+            //code
+            break;
+        
+        case "broken clouds":
+            //code
+            break;
+
+        case "overcast clouds":
+            //code 
+            break;
+    
+        default:
+            console.log('no cloud options detected')
             break;
     }
 }
